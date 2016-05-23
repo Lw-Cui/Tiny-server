25870245ead5183c42072a3b1a1caea08f495db9
lw <cui@hellolw.com>
2016-05-23 13:28:18 +0800

    add test

--- test/Main.cpp
+++ test/Main.cpp
@@ -1,14 +1,18 @@
 #include <Server.hpp>
+#include <thread>
+
+void startServe(unsigned short connfd) {
+	Server bop{connfd}; std::string buf;
+	while (bop.read_str(buf)) bop.write_str(buf);
+	bop.closeConnfd();
+}
+
 int main(int argc, char *argv[]) {
 	unsigned short port = 2000;
 	if (argc == 2) sscanf(argv[1], "%hu", &port);
 	try {
-		Server bop{port};
-		while (true) {
-			//json_map buf{json_data{bop.read_str()}};
-			//bop.write_str(buf.to_string());
-			bop.execute_CGI("CGI", bop.read_str());
-		}
+		while(true) 
+			std::thread(startServe, Server::getConnfd(port)).detach();
 	} catch(std::exception &e) {
 		std::cout << e.what() << std::endl;
 	}
